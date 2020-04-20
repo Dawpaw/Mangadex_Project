b7c02914a345aa8e4744eda290da9bf046c50c2e
Dawpaw <16710770+Dawpaw@users.noreply.github.com>
2020-04-19 19:32:39 -0500

    Changed Everything to request-html, but still need to solve render

--- mangadexProject.py
+++ mangadexProject.py
@@ -1,8 +1,10 @@
-from bs4 import BeautifulSoup as bs
-import requests
+
+from requests_html import HTML, HTMLSession
 import os
 
-def getManga_Soup():
+session = HTMLSession()
+
+def getManga_Html():
     ''' Get the manga html '''
     mangadex_url = 'https://mangadex.org/title/'
     mangaid = input("Manga Id:  \n")
@@ -10,10 +12,8 @@ def getManga_Soup():
     url = url.replace(' ', '')
     print(url)
     try:
-        mrequests = requests.get(url).text
-        manga_soup = bs(mrequests, 'lxml')
-        #print(manga_soup.prettify())
-        return manga_soup
+        mrequests = session.get(url).html
+        return mrequests
     except:
         return(print("Wrong Id"))
 
@@ -24,78 +24,101 @@ def find_Chapters():
     Corregir Solo descarga la primera página del los capitulos
     Para mangas muy largos
     '''
-    soup = getManga_Soup()
+    soup = getManga_Html()
     chaptersList = []
-    for chapter in soup.find_all('div', class_='row no-gutters'):
-        subchapters = chapter.find('div',
-        class_='chapter-row d-flex row no-gutters p-2 align-items-center border-bottom odd-row')
-        try:
-            sub_Subchapters = subchapters.find('div',
-            class_='col col-lg-5 row no-gutters align-items-center flex-nowrap text-truncate pr-1 order-lg-2')
-            chap_link = sub_Subchapters.find('a',href = True)
-            if chap_link.has_attr('href'):
-                chap_Newlink = "https://mangadex.org/" + chap_link['href']
-                print(chap_Newlink)
-                chaptersList.append(chap_Newlink)
-            print("\n---------------------------------------------------\n")
-        except:
+    for chapter in soup.find('a[href^="/chapter"]'):
+        subchapter = str(chapter.attrs['href'])
+        if subchapter.endswith('comments'):
             pass
+        else:
+            chaptersList.append('https://mangadex.org' + subchapter)
     chaptersList.reverse()
     return chaptersList
 
-def chapterSoup(chapter_url):
-    '''Get Chapter Soup'''
+def chapterHtml(chapter_url):
+    '''Get Chapter Soup
+        RENDER NOT WORKING
+    '''
     try:
-        reqNumber = requests.get(chapter_url).text
-        return bs(reqNumber, 'lxml')
+        r =session.get(chapter_url).html
+        r.render()
+        print(r)
+        return r
     except:
+        print("Couldnt print r")
         return None
 def compare_url(chapter_url):
-    '''
+    ''' (str) -> bool
     Compares if the current url is equal to the given url to check if
     the chapter is still the same or if it changed automatically
     changes the chapter automatically
     '''
+    currentUrl = ''
+    soup = chapterHtml(chapter_url)
 
-    soup = chapterSoup(chapter_url)
-    try:
-        url = soup.head.find('meta',{'property': 'og:url'})['content']
-        return url
-    except:
-        print("Probably the last chapter")
-        return None
+    urls = soup.find('head meta')
+    for url in urls:
+        try:
+            if url.attrs['property'] == 'og:url':
+                currentUrl = url.attrs['content']
+        except:
+            pass
+    if chapter_url.startswith(currentUrl):
+        print("Both Urls are the same")
+        return True
+    else:
+        chapter_url = currentUrl
+        return False
+    # try:
+    #     url = soup.head.find('meta',{'property': 'og:url'})['content']
+    #     return url
+    # except:
+    #     print("Probably the last chapter")
+    #     return None
 
 def get_image(chapter_soup):
-    for first_div in  chapter_soup.find_all('img'):
-        print(first_div.prettify())
-    '''first_div = chapter_soup.find('div',
-                class_='reader-main col row no-gutters flex-column flex-nowrap noselect')
-    second_div = first_div.find('div',
-    class_='reader-images col-auto row no-gutters flex-nowrap m-auto text-center cursor-pointer directional')
-    print(second_div.prettify())
+    soup = chapter_soup.find('img[srs^="https://s2.mangadex.org/data"]')
+    print(soup)
+
+  '''
+  Not Working
+
+  '''
+    # for first_div in  chapter_soup.find_all('img'):
+    #     print(first_div.prettify())
 
 
-'''
 
-'''def getTotalPages(chapter_url):
-     #Get the number of pages in a Chapter to create the
-    #appropiate number of Urls
 
-    chapter_soup = chapterSoup(chapter_url)
-    fDiv = chapter_soup.find('div', {'data-manga-id' : "46010"})
-    #sDiv = fDiv.find('div',
-    #          class_='col text-center reader-controls-page-text cursor-pointer')
-    print(fDiv.prettify())
 
-    #return number_Pages
-
-'''
+#     first_div = chapter_soup.find('div',
+#                 class_='reader-main col row no-gutters flex-column flex-nowrap noselect')
+#     second_div = first_div.find('div',
+#     class_='reader-images col-auto row no-gutters flex-nowrap m-auto text-center cursor-pointer directional')
+#     print(second_div.prettify())
+#
+#
+#
+#
+# def getTotalPages(chapter_url):
+#      #Get the number of pages in a Chapter to create the
+#     #appropiate number of Urls
+#
+#     chapter_soup = chapterSoup(chapter_url)
+#     fDiv = chapter_soup.find('div', {'data-manga-id' : "46010"})
+#     #sDiv = fDiv.find('div',
+#     #          class_='col text-center reader-controls-page-text cursor-pointer')
+#     print(fDiv.prettify())
+#
+#     #return number_Pages
+#
 
 
 r = find_Chapters()
 new = compare_url(r[0])
-new = new + "/1"
-msoup = chapterSoup(new)
+print(r[0])
+
+msoup = chapterHtml(new)
 get_image(msoup)
 #print("r is " + str(r))
 
